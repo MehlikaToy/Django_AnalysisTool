332059f1d0d7a9424d10a24dc8d3005cebfdc47c
Calvin Yin <calvin_yin@umail.ucsb.edu>
2015-07-29 10:09:36 -0700

    don't mind me

--- markov/getAPI.py
+++ markov/getAPI.py
@@ -2,8 +2,18 @@ import urllib2
 import json
 import pprint
 
-# pp = pprint.PrettyPrinter(indent=4)
+pp = pprint.PrettyPrinter(indent=4)
 
 def stuff():
-    request = urllib2.urlopen("https://api.typeform.com/v0/form/TOsSUX?key=6289137c9f94850a29203f5fd601c826a202d259&completed=true")
-    print json.dumps(request)
+    response = urllib2.urlopen("https://api.typeform.com/v0/form/TOsSUX?key=6289137c9f94850a29203f5fd601c826a202d259&completed=true&offset=0&limit=1?order_by[]=completed&order_by[]=date_land,desc")
+    data = json.loads(response.read())
+    # pp.pprint(data)
+
+    questionDict = {}
+    for question in data.get("questions"):
+        questionDict[str(question.get('id'))] = question.get('question')
+
+    print questionDict
+
+    # for answer in
+    print data['responses'][0].get("answers")
