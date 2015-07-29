c44b5dc46b8718bf8ec26756f64a3aa13b197c75
Calvin Yin <calvin_yin@umail.ucsb.edu>
2015-07-29 11:53:11 -0700

    wheee progress

--- markov/views.py
+++ markov/views.py
@@ -29,21 +29,3 @@ def resultsView(request):
         })
     return HttpResponse(template.render(context))
     # return render_to_response('markov/results.html', locals(), context_instance = RequestContext(request))
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            print form.cleaned_data['your_name']
-            # redirect to a new URL:
-            return HttpResponseRedirect('/markov')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
