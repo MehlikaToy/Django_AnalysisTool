b6c80b5cdfcbf46daa924ed14cac9d3ddace77f3
Jasmine Kim <jasminekim95@yahoo.com>
2015-08-21 16:24:09 -0700

    databases changed

--- Django_AnalysisTool/settings.py
+++ Django_AnalysisTool/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'Django_AnalysisTool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
