75b66ab16b13a263cf3a8099b11fa38f7fe78c43
Jasmine Kim <jasminekim95@yahoo.com>
2015-08-21 16:16:53 -0700

    dj static?

--- Django_AnalysisTool/wsgi.py
+++ Django_AnalysisTool/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_AnalysisTool.settings")
 
 
-from dj_static import Cling
+from dj-static import Cling
 
 application = Cling(get_wsgi_application())
 
