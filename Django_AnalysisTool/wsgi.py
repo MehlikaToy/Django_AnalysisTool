3484b53efb20fd35cb9329b6c2886129242932dc
Jasmine Kim <jasminekim95@yahoo.com>
2015-08-21 16:21:45 -0700

    can u pls wek

--- Django_AnalysisTool/wsgi.py
+++ Django_AnalysisTool/wsgi.py
@@ -8,13 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_AnalysisTool.settings")
 
-
-from dj-static import Cling
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 application = Cling(get_wsgi_application())
 
