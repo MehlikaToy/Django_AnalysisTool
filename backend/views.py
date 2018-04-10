2b80637081fb8be6665950d90550deb120375bd8
synthmed <tejpalv8@gmail.com>
2018-04-10 16:35:03 -0700

    a

--- backend/views.py
+++ backend/views.py
@@ -95,7 +95,7 @@ def resultsView(request):
             entry += [str(round(cirr_data[i+1][1], 2))+"%",
                       str(round(cirr_data[i+1][2], 2))+"%"]
 
-        if (i=20):
+        if (i==20):
             deathDiff = str(hbv_data[i+1][2] - hbv_data[i+1][1])
             tableArr.append([deathDiff, deathDiff, deathDiff, deathDiff, deathDiff])
         tableArr.append(entry)
