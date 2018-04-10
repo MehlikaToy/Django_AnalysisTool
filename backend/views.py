8b3499d50f0c76464992b6af0ee669be6fcb80c1
synthmed <tejpalv8@gmail.com>
2018-04-10 16:54:40 -0700

    a

--- backend/views.py
+++ backend/views.py
@@ -98,7 +98,7 @@ def resultsView(request):
 
         tableArr.append(entry)
 
-        deathDiff = str(hcc_data[0][2] - hcc_data[0][1])
+        deathDiff = str(hcc_data[21][2] - hcc_data[21][1])
         tableArr.append([deathDiff, deathDiff, deathDiff, deathDiff, deathDiff])
 
         
