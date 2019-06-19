3f2f66a5f0d2daa9647488aeedc4fd60fe7ea76a
SidNath21 <37987542+SidNath21@users.noreply.github.com>
2019-06-19 15:18:32 -0700

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -105,7 +105,7 @@ def resultsView(request):
 
     # Generate recommendation. //    recommendation = flow.getWhoRec(cirr, age, ALT, HBV_DNA)
     recomendation = 'Monitoring'
-    if (cirr  == 'Yes' || ALT == 'Persistently Abnormal' and HBV_DNA == '>20,000 IU/ml'):
+    if (cirr  == 'Yes' or ALT == 'Persistently Abnormal' and HBV_DNA == '>20,000 IU/ml'):
         recomendation += ' and Treatment'
     whoRec = 'You Need ' + recommendation
     t_heading = recommendation
