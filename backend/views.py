acba3ceebf947b94edd96ec98d9b93b1e9045b43
SidNath21 <37987542+SidNath21@users.noreply.github.com>
2019-06-26 14:28:05 -0700

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -89,13 +89,13 @@ def resultsView(request):
    
     for i in sample_indices:
         entry = [i,
-                 str(round(hbv_data[i+1][1]), 1)+"%",
-                 str(round(hbv_data[i+1][2]), 1)+"%",
-                 str(round(hcc_data[i+1][1]), 1)+"%",
-                 str(round(hcc_data[i+1][2]), 1)+"%"]
+                 str(round(hbv_data[i+1][1], 1))+"%",
+                 str(round(hbv_data[i+1][2], 1))+"%",
+                 str(round(hcc_data[i+1][1], 1))+"%",
+                 str(round(hcc_data[i+1][2], 1))+"%"]
         if (cirr != 'Yes'):
-            entry += [str(round(cirr_data[i+1][1]), 1)+"%",
-                      str(round(cirr_data[i+1][2]), 1)+"%"]
+            entry += [str(round(cirr_data[i+1][1], 1))+"%",
+                      str(round(cirr_data[i+1][2], 1))+"%"]
 
 
         tableArr.append(entry)
